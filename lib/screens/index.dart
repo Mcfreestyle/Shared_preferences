ebfad964dc1f65d20bf2dc855a17d32ede57366c
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-11-22 22:59:53 -0500

    feat: added exportation(login.dart)

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -1,3 +1,4 @@
 export 'package:sesion9/screens/home.dart';
 export 'package:sesion9/screens/config.dart';
 export 'package:sesion9/screens/profile.dart';
+export 'package:sesion9/screens/login.dart';
